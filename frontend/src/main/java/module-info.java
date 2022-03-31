a63b65df797062eac63e95d37501d12538db89eb
Einar Sandström <eisa1001@student.miun.se>
2022-03-31 10:23:18 +0200

    Changed pom file to import StanfordCoreNLP and added sentimentAnalyser that Fredrik built

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -10,7 +10,11 @@ module com.dt002g.reviewapplication.frontend {
 	requires javafx.graphics;
 	requires java.sql;
 	requires retrofit2.converter.jackson;
+	requires stanford.corenlp;
+	requires ejml.simple;
     opens com.dt002g.reviewapplication.frontend to javafx.fxml;
     exports com.dt002g.reviewapplication.frontend;
     exports com.dt002g.reviewapplication.frontend.service;
+    exports com.dt002g.reviewapplication.frontend.sentimentanalyser;
+    
 }
