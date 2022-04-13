876ea2b8ccbc7b1edce6344c7e07e14a576a083b
EinarSandstrom1 <eisa1001@student.miun.se>
2022-04-13 22:21:51 +0200

    Constrainted CSV import to not impor rows were freetext longer than 2000 characters

---
+++ frontend/src/main/java/com/dt002g/reviewapplication/frontend/service/SentenceBackendEntity.java
@@ -0,0 +1,116 @@
+/*package com.dt002g.reviewapplication.frontend.service;
+
+public class SentenceBackendEntity {
+
+    Long id;
+    String text;
+    double veryPositive;
+    double positive;
+    double neutral;
+    double negative;
+    double veryNegative;
+    int score;
+    List<SentenceToAdjective> sentenceToAdjectives;
+    Review review;
+
+    public Sentence() {
+
+    }
+
+    public Sentence(String text, List<Double> grades, int score) {
+        this.text = text;
+        this.score = score;
+        if(grades.size() == 5) {
+            this.veryPositive = grades.get(0);
+            this.positive = grades.get(1);
+            this.neutral = grades.get(2);
+            this.negative = grades.get(3);
+            this.veryNegative = grades.get(4);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public double getVeryPositive() {
+        return veryPositive;
+    }
+
+    public void setVeryPositive(double veryPositive) {
+        this.veryPositive = veryPositive;
+    }
+
+    public double getPositive() {
+        return positive;
+    }
+
+    public void setPositive(double positive) {
+        this.positive = positive;
+    }
+
+    public double getNeutral() {
+        return neutral;
+    }
+
+    public void setNeutral(double neutral) {
+        this.neutral = neutral;
+    }
+
+    public double getNegative() {
+        return negative;
+    }
+
+    public void setNegative(double negative) {
+        this.negative = negative;
+    }
+
+    public double getVeryNegative() {
+        return veryNegative;
+    }
+
+    public void setVeryNegative(double veryNegative) {
+        this.veryNegative = veryNegative;
+    }
+
+    public Review getReview() {
+        return this.review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public List<SentenceToAdjective> getSentenceToAdjectives() {
+        return sentenceToAdjectives;
+    }
+
+    public void setSentenceToAdjectives(List<SentenceToAdjective> sentenceToAdjectives) {
+        this.sentenceToAdjectives = sentenceToAdjectives;
+    }
+
+
+
+
+
+}*/
